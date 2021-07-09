03bc2d3d85b31bc8b4e5a971acd33260f7fb2e46
rgunjote <rahul.gunjote@lorhanit.com>
2021-07-09 10:44:19 +1000

    Docker build final

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -10,7 +10,10 @@ namespace Catalog.Settings
         public string ConnectionString { 
             get {
                return $"mongodb://{User}:{Password}@{Host}:{Port}";
+               //return "mongodb+srv://root-node:omD5Paeviacso4uu@cluster0.91ff9.mongodb.net";
             } 
+
+           // mongodb+srv://root-node:<password>@cluster0.91ff9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
         }
     }
 }
